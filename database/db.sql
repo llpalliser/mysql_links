59280fdd795ace41fe194c8a9da7354312324854
Llorenç Palliser <palliser@palliser.es>
2020-12-28 08:18:44 +0100

    done

---
+++ database/db.sql
@@ -0,0 +1,46 @@
+
+--// USERS TABLE 
+
+CREATE DATABASE database_links;
+
+USE database_links;
+
+CREATE TABLE users(
+    id INT (11) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+
+);
+
+ALTER TABLE users
+    ADD PRIMARY KEY (id);
+
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
+
+-- // LINKS TABLE
+
+CREATE TABLE links(
+    id INT(11) NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT (11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
+
+-- notes
+-- https://www.configserverfirewall.com/ubuntu-linux/enable-mysql-remote-access-ubuntu/#:~:text=Enable%20MySQL%20Server%20Remote%20Connection,in%20the%20MySQL%20Configuration%20File.
---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    database: {
+        host: 'localhost',
+        user: 'admin',
+        password: 'mysql@159357!',
+        database: 'database_links'
+    }
+}
